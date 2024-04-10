c0751a5699f4a0f273325cb8366cb9d488ca3daf
galjeza <gal.jeza@protonmail.com>
2024-04-10 12:35:11 +0200

    vors

--- api/index.js
+++ api/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+// Use CORS for all routes
+app.use(cors());
 
 app.post("/", (req, res) => {
   console.log("Cookies: ", req.body);
